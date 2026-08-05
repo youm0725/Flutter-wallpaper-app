893ca9812b474b88680d693a9d8c052d2f0aad96
Youm Patel <youm250707@gmail.com>
2026-08-06 00:03:14 +0530

    fix: auto-navigate from splash screen to home screen

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import '../../core/constants/app_colors.dart';
 import '../../core/constants/app_sizes.dart';
+import '../../core/router/route_constants.dart';
 
-/// Minimal, elegant Splash Screen placeholder.
-class SplashScreen extends StatelessWidget {
+/// Minimal, elegant Splash Screen that navigates to HomeScreen after initialization.
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  Future<void> _navigateToHome() async {
+    await Future<void>.delayed(const Duration(milliseconds: 1500));
+    if (mounted) {
+      context.go(RouteConstants.homePath);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
