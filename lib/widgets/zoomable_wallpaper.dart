68b47f0cd829dc33bce47a4b40a3514d6526e931
Youm Patel <youm250707@gmail.com>
2026-08-08 00:29:43 +0530

    Updated wallpaper gallery assets, categories & metadata via Asset Manager

--- lib/widgets/zoomable_wallpaper.dart
+++ lib/widgets/zoomable_wallpaper.dart
@@ -70,7 +70,7 @@ class _ZoomableWallpaperState extends State<ZoomableWallpaper>
             tag: 'wallpaper_${widget.wallpaper.id}',
             child: Image.asset(
               widget.wallpaper.imagePath,
-              fit: BoxFit.contain,
+              fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
               frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
